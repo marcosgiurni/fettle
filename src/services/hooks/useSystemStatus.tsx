2a6d54ab8a066b01a578b7327d2ac7cf4aaab3b8
Marcos Giurni <viniciusgiurni@hotmail.com>
2023-06-30 11:17:54 -0300

    Update useSystemStatus.tsx

--- src/services/hooks/useSystemStatus.tsx
+++ src/services/hooks/useSystemStatus.tsx
@@ -29,25 +29,25 @@ function useSystemStatus() {
                 
                 if (services.every((item) => item.status === "success")) {
                     setSystemStatus({
-                        title: "All System Operational",
+                        title: "Todos os sistemas operantes",
                         status: Status.OPERATIONAL,
                         datetime: services[0].date
                     });
                 } else if (services.every((item) => item.status === "failed")) {
                     setSystemStatus({
-                        title: "Outage",
+                        title: "Indisponível",
                         status: Status.OUTAGE,
                         datetime: services[0].date
                      });
                 } else if (services.every((item) => item.status === "")) {
                     setSystemStatus({
-                        title: "Unknown",
+                        title: "Desconhecido",
                         status: Status.UNKNOWN,
                         datetime: services[0].date
                     });
                 } else {
                     setSystemStatus({
-                        title: "Partial Outage",
+                        title: "Indisponibilidade Parcial",
                         status: Status.PARTIAL_OUTAGE,
                         datetime: services[0].date
                     });
